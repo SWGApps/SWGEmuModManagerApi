1961cf935b5532a8c3ab6a641f7d757627a79fa9
1Sudo <zjones89@outlook.com>
2022-03-16 22:01:12 -0500

    [Added] Supporting code for page size selection

--- Controllers/ModsController.cs
+++ Controllers/ModsController.cs
@@ -12,6 +12,17 @@ public class ModsController : ControllerBase
     public async Task<IActionResult> Get(int inputPageNumber, int inputPageSize, int sortType, int sortOrder, string filterValue)
     {
         IEnumerable<Mod> mods = await Mod.GetMods(sortType, sortOrder, filterValue);
+
+        switch (inputPageSize)
+        {
+            case 0: inputPageSize = 10; break;
+            case 1: inputPageSize = 20; break;
+            case 2: inputPageSize = 30; break;
+            case 3: inputPageSize = 40; break;
+            case 4: inputPageSize = 50; break;
+            default: inputPageSize = 10; break;
+        }
+
         IPagedList<Mod> modList = mods.ToPagedList(inputPageNumber, inputPageSize);
 
         return Ok(new PaginatedResponse<IPagedList<Mod>>(
--- Models/PaginatedResponse.cs
+++ Models/PaginatedResponse.cs
@@ -1,37 +1,35 @@
-﻿namespace SWGEmuModManagerApi.Models
+﻿namespace SWGEmuModManagerApi.Models;
+
+public class PaginatedResponse<T> : Response<T>
 {
-    public class PaginatedResponse<T> : Response<T>
+    public PaginatedResponse(T? data, int totalItemCount, int pageSize, int pageNumber, int pageCount, bool isLastPage, 
+        bool isFirstPage, int lastItemOnPage, int firstItemOnPage, bool hasPreviousPage, bool hasNextPage)
     {
-        public PaginatedResponse(T? data, int totalItemCount, int pageSize, int pageNumber, int pageCount, bool isLastPage, 
-            bool isFirstPage, int lastItemOnPage, int firstItemOnPage, bool hasPreviousPage, bool hasNextPage)
-        {
-            HasNextPage = hasNextPage;
-            HasPreviousPage = hasPreviousPage;
-            FirstItemOnPage = firstItemOnPage;
-            LastItemOnPage = lastItemOnPage;
-            IsFirstPage = isFirstPage;
-            IsLastPage = isLastPage;
-            PageCount = pageCount;
-            PageNumber = pageNumber;
-            PageSize = pageSize;
-            TotalItemCount = totalItemCount;
+        HasNextPage = hasNextPage;
+        HasPreviousPage = hasPreviousPage;
+        FirstItemOnPage = firstItemOnPage;
+        LastItemOnPage = lastItemOnPage;
+        IsFirstPage = isFirstPage;
+        IsLastPage = isLastPage;
+        PageCount = pageCount;
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        TotalItemCount = totalItemCount;
 
-            Succeeded = true;
-            Message = string.Empty;
-            Errors = null;
-            Data = data;
-        }
-
-        public bool HasNextPage { get; set; }
-        public bool HasPreviousPage { get; set; }
-        public int FirstItemOnPage { get; set; }
-        public int LastItemOnPage { get; set; }
-        public bool IsFirstPage { get; set; }
-        public bool IsLastPage { get; set; }
-        public int PageCount { get; set; }
-        public int PageNumber { get; set; }
-        public int PageSize  { get; set; }
-        public int TotalItemCount { get; set; }
-
+        Succeeded = true;
+        Message = string.Empty;
+        Errors = null;
+        Data = data;
     }
+
+    public bool HasNextPage { get; set; }
+    public bool HasPreviousPage { get; set; }
+    public int FirstItemOnPage { get; set; }
+    public int LastItemOnPage { get; set; }
+    public bool IsFirstPage { get; set; }
+    public bool IsLastPage { get; set; }
+    public int PageCount { get; set; }
+    public int PageNumber { get; set; }
+    public int PageSize  { get; set; }
+    public int TotalItemCount { get; set; }
 }
